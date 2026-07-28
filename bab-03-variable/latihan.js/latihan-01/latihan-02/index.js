65e8c664dabedb3550b086a1ce84230fccbf6f4b
sakhisan-28 <sanjisansan42@gmail.com>
2026-07-28 11:03:10 +0700

    Menambahkan latihan-01 bab-03 variable

---
+++ bab-03-variable/latihan.js/latihan-01/latihan-02/index.js
@@ -0,0 +1,15 @@
+
+const output = '===== BIODATA ====='
+const Nama : 'Sakhi san';
+let Umur : '18 tahun';
+const kota : 'Sentul City, Bogor';
+const pekerjaan : 'Pelajaar';
+
+//output
+console.log(output);
+//Nama
+console.log(Nama);
+//Umur
+console.log(kota);
+//pekerjaan
+console.log(pekerjaan);
